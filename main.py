ff78eafbf1e17e4695bcfb3a02a30b9be8002c12
Obaada <159489903+Obaada@users.noreply.github.com>
2024-03-06 00:38:55 +0000

    done

---
+++ main.py
@@ -0,0 +1,40 @@
+from art import logo 
+from art import vs 
+from game_data import data
+import random
+import os
+
+def GeneratePlayer():
+  RandomAccount = random.choice(data)
+  return RandomAccount
+
+def CheckWinner(A, B):
+  if A > B:
+    return "A"
+  elif B > A: 
+    return "B"
+  else: return "D"
+
+
+PlayerLost = False
+PlayerScore = 0
+RandomA = GeneratePlayer()
+RandomB = GeneratePlayer()
+while not PlayerLost:
+  print(f"hmmmm A:{RandomA['follower_count']}, B:{RandomB['follower_count']}")
+  Winner = CheckWinner(RandomA['follower_count'], RandomB['follower_count'])
+  os.system('clear')
+  print(logo)
+  print(f"Compare A: {RandomA['name']}, a {RandomA['description']}, from {RandomA['country']}.")
+  print(vs)
+  print(f"Against B: {RandomB['name']}, a {RandomB['description']}, from {RandomB['country']}.")
+  Answer = input("Who has more followers? Type 'A' or 'B':")
+  if Answer == Winner or Winner == "D":
+    PlayerScore += 1
+    RandomA = RandomB
+    RandomB = GeneratePlayer()
+  else: 
+    os.system('clear')
+    print(logo)
+    print(f"Sorry, that's wrong. Final score: {PlayerScore}")
+    PlayerLost = True
