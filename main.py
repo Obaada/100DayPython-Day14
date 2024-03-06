5f0af787f4372fec6032d8b7f67903d917894dde
Obaada <159489903+Obaada@users.noreply.github.com>
2024-03-06 00:44:34 +0000

    done2

--- main.py
+++ main.py
@@ -15,26 +15,28 @@ def CheckWinner(A, B):
     return "B"
   else: return "D"
 
-
-PlayerLost = False
-PlayerScore = 0
-RandomA = GeneratePlayer()
-RandomB = GeneratePlayer()
-while not PlayerLost:
-  print(f"hmmmm A:{RandomA['follower_count']}, B:{RandomB['follower_count']}")
-  Winner = CheckWinner(RandomA['follower_count'], RandomB['follower_count'])
-  os.system('clear')
-  print(logo)
-  print(f"Compare A: {RandomA['name']}, a {RandomA['description']}, from {RandomA['country']}.")
-  print(vs)
-  print(f"Against B: {RandomB['name']}, a {RandomB['description']}, from {RandomB['country']}.")
-  Answer = input("Who has more followers? Type 'A' or 'B':")
-  if Answer == Winner or Winner == "D":
-    PlayerScore += 1
-    RandomA = RandomB
-    RandomB = GeneratePlayer()
-  else: 
+def game():  
+  PlayerLost = False
+  PlayerScore = 0
+  RandomA = GeneratePlayer()
+  RandomB = GeneratePlayer()
+  while not PlayerLost:
     os.system('clear')
+    print(f"hmmmm A:{RandomA['follower_count']}, B:{RandomB['follower_count']}")
+    Winner = CheckWinner(RandomA['follower_count'], RandomB['follower_count'])
     print(logo)
-    print(f"Sorry, that's wrong. Final score: {PlayerScore}")
-    PlayerLost = True
+    print(f"Compare A: {RandomA['name']}, a {RandomA['description']}, from {RandomA['country']}.")
+    print(vs)
+    print(f"Against B: {RandomB['name']}, a {RandomB['description']}, from {RandomB['country']}.")
+    Answer = input("Who has more followers? Type 'A' or 'B':")
+    if Answer == Winner or Winner == "D":
+      PlayerScore += 1
+      print(f"Good Job! Your score is now : {PlayerScore}")
+      RandomA = RandomB
+      RandomB = GeneratePlayer()
+    else: 
+      os.system('clear')
+      print(logo)
+      print(f"Sorry, that's wrong. Final score: {PlayerScore}")
+      PlayerLost = True
+game()  
